0aabd9f6197f72b2a16425913f2f41faf6ccaa52
TruyenBD <mrtruyenbd1407@gmail.com>
2025-01-12 00:49:13 +0700

    feat: config eslint

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { useCounter } from '@/store/counter'
+import { isDark, toggleDark } from '@/composables/dark'
+
 const counterStore = useCounter()
 
 </script>
@@ -8,4 +10,15 @@ const counterStore = useCounter()
     Hello world
   </div>
   <span>{{ counterStore.myRef }}</span>
+  <div class="">
+    <ClientOnly>
+                <button
+                  class="bg-transparent px-2 py-1 focus:outline-transparent rounded-sm bg-white dark:bg-black text-black dark:text-white rounded-md hover:text-primary"
+                  @click="(e) => toggleDark()"
+                >
+                  <carbon:moon class="h-5 w-5" v-if="isDark" />
+                  <carbon:sun class="h-5 w-5" v-else />
+                </button>
+              </ClientOnly>
+            </div>
 </template>

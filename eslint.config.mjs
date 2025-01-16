c9b14e615c6a9bae0ec6fe3a0b0505be2700f5ca
TruyenBD <mrtruyenbd1407@gmail.com>
2025-01-16 23:00:18 +0700

    feat: config eslint antfu

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,23 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
 import antfu from '@antfu/eslint-config'
 
-export default withNuxt(
-  antfu({
-    files: ['**/*.ts', '**/*.vue'],
-    typescript: {
-      overrides: {
-        'no-var': 'off',
-      },
-    },
-    vue: true,
-    jsonc: true,
-    stylistic: {
-      indent: 2,
-      quotes: 'single',
-    },
-    rules: {
-      'no-console': 'off',
-      'no-debugger': 'warn',
+export default antfu({
+  // stylistic: true, // enable stylistic formatting rules
+  typescript: true,
+  vue: true,
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
 
-      'node/prefer-global/process': 'off',
+    'node/prefer-global/process': 'off',
 
-      'regexp/no-unused-capturing-group': 'off',
+    'regexp/no-unused-capturing-group': 'off',
 
-      'ts/ban-ts-comment': 'off',
-      'ts/no-empty-object-type': 'off',
-      'ts/no-unused-expressions': ['error', { allowTaggedTemplates: true }],
+    'ts/ban-ts-comment': 'off',
+    'ts/no-empty-object-type': 'off',
+    'ts/no-unused-expressions': ['error', { allowTaggedTemplates: true }],
 
-      'vue/valid-v-slot': 'off',
-      'vue/custom-event-name-casing': 'off',
-      'vue/no-multiple-template-root': 'off',
-    },
-  })
-)
+    'vue/valid-v-slot': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'vue/no-multiple-template-root': 'off',
+  },
+})
